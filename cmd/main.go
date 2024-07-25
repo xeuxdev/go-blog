c39b34c3d786c6dd11dc1927b9777ff9f6ede491
xeuxdev <tochukwujohn24@gmail.com>
2024-07-25 12:56:56 +0100

    edit main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/xeuxdev/go-blog/internal/server"
+
 func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 	println("Hello World!!!!")
 }
